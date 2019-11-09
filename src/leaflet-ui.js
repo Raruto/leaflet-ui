4aaa10edf66e116bdfd2496c6624e06e63a32907
Raruto <Theo.95@hotmail.it>
2019-11-09 15:27:03 +0100

    add "leaflet-edit-osm" as an external dependency

--- src/leaflet-ui.js
+++ src/leaflet-ui.js
@@ -3,6 +3,7 @@ import 'leaflet.locatecontrol';
 import 'leaflet.fullscreen';
 import 'leaflet-pegman';
 import '@raruto/leaflet-gesture-handling';
+import '@raruto/leaflet-edit-osm';
 import 'leaflet-control-layers-inline';
 import 'leaflet-minimap';
 import 'leaflet-loading';
@@ -616,36 +617,6 @@ var lazyLoader = {
 				this._handligMiniMapTypeToggle = false;
 			}
 		},
-	});
-
-	// Custom open in OSM Edit link.
-	L.Control.EditInOSM = L.Control.extend({
-		options: {
-			editor: false, // eg: "id", "potlatch2" or "remote"
-		},
-		_edit: function() {
-			var center = this._map.getCenter();
-			var z = this._map.getZoom();
-			var editor = this.options.editor ? '&editor=' + this.options.editor : '';
-			window.open('http://www.openstreetmap.org/edit?' + 'zoom=' + z + editor + '&lat=' + center.lat + '&lon=' + center.lng);
-		},
-		onAdd: function(map) {
-			var container = L.DomUtil.create('div', 'leaflet-control-attribution leaflet-edit-osm'),
-				link = L.DomUtil.create('a', '', container);
-
-			link.href = '#';
-			link.innerHTML = '✎ Edit';
-			link.title = 'Edit in OpenStreetMap';
-
-			L.DomEvent
-				.on(link, 'click', L.DomEvent.stopPropagation)
-				.on(link, 'mousedown', L.DomEvent.stopPropagation)
-				.on(link, 'dblclick', L.DomEvent.stopPropagation)
-				.on(link, 'click', L.DomEvent.preventDefault)
-				.on(link, 'click', L.bind(this._edit, this), this);
-
-			return container;
-		}
 	});
 
 	// Check if an array contains any element from another one.
